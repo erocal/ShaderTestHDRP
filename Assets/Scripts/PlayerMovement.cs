6ea79725e1ef64456539109d1eaabac947e606ed
erocal <s159752zxcvbn1@gmail.com>
2024-07-28 01:18:49 +0800

    [!][+][-]右邊書櫃

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,12 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed = 5f;
 
+    public float rotationSpeed = 100f;
+    public float verticalRotationSpeed = 100f;
+
+    private float verticalRotationLimit = 80f;
+    private float verticalRotation = 0f;
+
     #endregion
 
     #region -- 變數參考區 --
@@ -18,9 +24,9 @@ public class PlayerMovement : MonoBehaviour
     #region -- 初始化/運作 --
 
     private void Awake()
-	{
-		
-	}
+    {
+
+    }
 
     void Update()
     {
@@ -29,15 +35,46 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");
 
         // 計算移動
-        Vector3 movement = new Vector3(moveX, 0, moveY) * moveSpeed * Time.deltaTime;
+        Vector3 movement = (transform.forward * moveY + transform.right * moveX) * moveSpeed * Time.deltaTime;
+
+        movement.y = 0f;
 
         // 應用移動
         transform.Translate(movement, Space.World);
+
+        Rotation();
     }
 
     #endregion
 
     #region -- 方法參考區 --
+
+    private void Rotation()
+    {
+        // 鍵盤控制
+        float rotation = 0f;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotation = -rotationSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            rotation = rotationSpeed * Time.deltaTime;
+        }
+
+        // 滑鼠控制水平旋轉
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+
+        // 滑鼠控制垂直旋轉
+        float mouseY = Input.GetAxis("Mouse Y") * verticalRotationSpeed * Time.deltaTime;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
+
+        // 應用旋轉
+        transform.Rotate(Vector3.up, rotation + mouseX, Space.World);
+        transform.localEulerAngles = new Vector3(verticalRotation, transform.localEulerAngles.y, 0f);
+
+    }
 
     #endregion
 
